baecff0b29c12b90f5fcee8ec739c0a4fab5204b
ripkenb <bramanri@gmail.com>
2026-05-23 16:37:35 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+const teams = [
+  {
+    name: "Angry Apples",
+    owner: "Ripken",
+    logo: "assets/logos/angry-apples.png.png",
+    record: "4-10",
+    titles: 0,
+    pointsFor: 1375
+  }
+];
