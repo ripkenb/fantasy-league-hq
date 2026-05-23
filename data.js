34a58d029415fa7f8405382727c566f7bb6b15ca
ripkenb <bramanri@gmail.com>
2026-05-23 16:48:10 -0700

    Update data.js

--- data.js
+++ data.js
@@ -16,5 +16,77 @@ const teams = [
     record: "11-3",
     titles: 1,
     pointsFor: 1878
+  },
+
+  {
+    name: "Big Ben Banditos",
+    owner: "Ben",
+    logo: "assets/logos/big-ben-banditos.png.png",
+    record: "8-6",
+    titles: 0,
+    pointsFor: 1641
+  },
+
+  {
+    name: "Bryley the Builder",
+    owner: "Bryley",
+    logo: "assets/logos/bryley-the-builder.png.png",
+    record: "7-7",
+    titles: 0,
+    pointsFor: 1682
+  },
+
+  {
+    name: "The Boarder Patrollers",
+    owner: "Curtis",
+    logo: "assets/logos/boarder-patrollers.png.png",
+    record: "9-5",
+    titles: 0,
+    pointsFor: 1688
+  },
+
+  {
+    name: "Bradshaw Bunch",
+    owner: "Jeff",
+    logo: "assets/logos/bradshaw-bunch.png.png",
+    record: "7-7",
+    titles: 0,
+    pointsFor: 1565
+  },
+
+  {
+    name: "McLovin",
+    owner: "Rawlings",
+    logo: "assets/logos/mclovin.png.png",
+    record: "8-6",
+    titles: 0,
+    pointsFor: 1654
+  },
+
+  {
+    name: "Lake Cle Elum",
+    owner: "Ritch",
+    logo: "assets/logos/lake-cle-elum.png.png",
+    record: "9-5",
+    titles: 0,
+    pointsFor: 1672
+  },
+
+  {
+    name: "Purple Reign",
+    owner: "Willie",
+    logo: "assets/logos/purple-reign.png.png",
+    record: "0-14",
+    titles: 0,
+    pointsFor: 1074
+  },
+
+  {
+    name: "The Cat's Pajamas",
+    owner: "Reese",
+    logo: "assets/logos/the-cat's-pajamas.png",
+    record: "7-7",
+    titles: 0,
+    pointsFor: 1480
   }
 ];
