da8888264b347c7b86ed8a9f29f62a409417af69
ripkenb <bramanri@gmail.com>
2026-05-23 16:39:41 -0700

    Update data.js

--- data.js
+++ data.js
@@ -6,5 +6,16 @@ const teams = [
     record: "4-10",
     titles: 0,
     pointsFor: 1375
+  },
+
+  {
+    name: "Jake the Snake",
+    owner: "Jake",
+    logo: "assets/logos/jake-the-snake.png.png",
+    record: "11-3",
+    titles: 1,
+    pointsFor: 1878
   }
 ];
+
+console.log(teams);
